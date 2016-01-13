aa6189b2455b99b13546bc9fc610781b5c68353b
Tom Trabue <tom.trabue@gmail.com>
2016-01-12 22:12:56 -0500

    Added logic for copying Sublime prefs files to their correct locations

--- bin/environment.sh
+++ bin/environment.sh
@@ -3,7 +3,7 @@
 # Place all necessary folders and files on the filesystem for development
 
 # Local variables for referencing project folders:
-local repo_automation=../
+local repo_automation=".."
 local repo_copy="$repo_automation/copy"
 local repo_link="$repo_automation/link"
 local repo_source="$repo_automation/source"
@@ -18,3 +18,7 @@ mkdir ~/Sublime_Projects
 
 # Install Package Control plugin on the new Sublime installation:
 cp "$repo_copy/Package Control.sublime-package" ~/"Library/Application Support/Sublime Text 3/Installed Packages/Package Control.sublime-package"
+
+# Copy Sublime preference files:
+cp "$repo_copy/Package Control.sublime-settings" ~/"Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
+cp "$repo_copy/Preferences.sublime-settings" ~/"Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
